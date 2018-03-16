3e22ba0186920499d073cdeecfa5a0a5c936694f
Will Cheng <willchengsters@gmail.com>
2018-03-16 16:35:05 -0700

    intro

--- README.md
+++ README.md
@@ -14,9 +14,3 @@
 > 1. Start dev server
 >    - `npm start`
 > 1. Open a browser and navigate to `localhost:3000`
-### Bundle For Deployment
-
-> 1. Run webpack to bundle files
->    - `npm run bundle`
-> 
-> **NOTE:** *After bundling you can not directly run your app locally. You must run your app from the root directory of a server.*
--- src/index.js
+++ src/index.js
@@ -3,7 +3,18 @@ import ReactDOM from 'react-dom';
 
 import App from './components/app';
 
+const user = {
+    firstName: 'Will',
+    lastName: 'Cheng'
+}
+
+function greetUser(user){
+    return user.firstName + ' ' + user.lastName
+}
+
+const greeting = <h1> Hello {greetUser(user)}, wubbalu00 badubdub </h1>
+
 ReactDOM.render(
-    <App />,
+    greeting,
     document.getElementById('root')
 );
